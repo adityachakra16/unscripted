8224b6b107d8fc2dc6bc31bd1f6cc681d4669f1c
adityachakra16 <adityachakra16@gmail.com>
2023-10-17 17:57:05 +0530

    setup backend services

--- unscripted-backend/src/app.module.ts
+++ unscripted-backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ScriptsModule } from './scripts/scripts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ScriptsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
